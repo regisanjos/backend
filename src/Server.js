d849b78f224a4cc5ae86919d43c372bdee87ba07
regisanjos <regisanjos47@gmail.com>
2024-07-03 05:01:36 -0300

    deletei  cors

--- src/Server.js
+++ src/Server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 
 
 
@@ -7,7 +6,7 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors());
+
 app.use(express.json());
 
 
